a2f9c83c5d5b4270a1dd4c35d0d58c9c2f7c8c59
nufcfan <nufcfangold@googlemail.com>
2014-06-21 18:19:19 +0100

    socket.io

--- baked-in-a-pi-server/server.js
+++ baked-in-a-pi-server/server.js
@@ -3,13 +3,15 @@ var ds18b20 = require('ds18x20');
 
 var address = '/sys/bus/w1/devices/28-000005cc39f1/w1_slave';
 
-io.sockets.on('connection', function (socket) {
-    var temperatures = setInterval(function () {
-        ds18b20.get(function (err, temp) {
-            if (err) throw err;
-            console.log(temp);
-        });
-    }, 100);
+var clients = io
+    .of('/temperatures')
+    .on('connection', function (socket) {
+        var temperatures = setInterval(function () {
+            ds18b20.get(function (err, temp) {
+                if (err) throw err;
+                clients.emit(temp);
+            });
+        },1000);
 
     socket.on('disconnect', function () {
         clearInterval(temperatures);
