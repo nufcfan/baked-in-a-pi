3375ab85b8cd1426d3adddbb965bf9191fa22853
nufcfan <nufcfangold@googlemail.com>
2014-06-21 17:14:52 +0100

    added ds18x20 dependency for temps

--- baked-in-a-pi-server/server.js
+++ baked-in-a-pi-server/server.js
@@ -1,6 +1,17 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var io = require('socket.io').listen(80);
+var ds18b20 = require('ds18x20');
+
+var address = '/sys/bus/w1/devices/28-000005cc39f1/w1_slave';
+var sensor = new ds18b20(address);
+
+io.sockets.on('connection', function (socket) {
+    var temperatures = setInterval(function () {
+        sensor.read(function (data) {
+            console.log(data);
+        });
+    }, 100);
+
+    socket.on('disconnect', function () {
+        clearInterval(temperatures);
+    });
+});
